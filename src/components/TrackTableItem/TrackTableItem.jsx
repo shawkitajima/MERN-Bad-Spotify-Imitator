22b72c5733eb2cbff31a9cd4885b67f49084af62
Shaw Kitajima <shawkitajima@gmail.com>
2020-01-27 16:12:35 -0800

    housecleaning

--- src/components/TrackTableItem/TrackTableItem.jsx
+++ src/components/TrackTableItem/TrackTableItem.jsx
@@ -1,14 +1,10 @@
 import React from 'react';
-import styles from './TrackTableItem.module.css';
 import spotifyService from '../../utils/spotifyService';
 import PlaylistSelector from '../../components/PlaylistSelector/PlaylistSelector';
 import { formatTime } from '../../utils/utilities';
 import Button from '@material-ui/core/Button';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
-import playIcon from './play.png';
-import color from '@material-ui/core/colors/amber';
-import { findByLabelText } from '@testing-library/react';
 
 const TrackTableItem = props => {
     let time = formatTime(Math.floor(props.length / 1000))
